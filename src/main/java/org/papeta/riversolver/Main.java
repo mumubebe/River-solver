13e4760e3c305f0be5e6e9f9f3c6043e313df038
fabryprog <fabryprog@gmail.com>
2017-02-02 21:03:10 +0100

    maven integration

---
+++ src/main/java/org/papeta/riversolver/Main.java
@@ -0,0 +1,12 @@
+package org.papeta.riversolver;
+
+import org.papeta.riversolver.gui.RiverFrame;
+
+
+public class Main {
+
+	public static void main(String args[]){
+		RiverFrame mf = new RiverFrame();
+		mf.drawGUI();
+	}
+}
